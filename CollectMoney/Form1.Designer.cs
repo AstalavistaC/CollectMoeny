1b8afba48248a4ea99db72e5a184c7d7a17fc239
hassan Mattar <hassan.mattar.id@gmail.com>
2020-09-11 19:58:24 +0300

    first commit

---
+++ CollectMoney/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace CollectMoney
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt100 = new System.Windows.Forms.TextBox();
+            this.txt50 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt20 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt10 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl10 = new System.Windows.Forms.Label();
+            this.lbl20 = new System.Windows.Forms.Label();
+            this.lbl50 = new System.Windows.Forms.Label();
+            this.lbl100 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btninsert = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblserach = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "100";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt100
+            // 
+            this.txt100.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt100.Location = new System.Drawing.Point(133, 106);
+            this.txt100.Name = "txt100";
+            this.txt100.Size = new System.Drawing.Size(140, 35);
+            this.txt100.TabIndex = 1;
+            this.txt100.TextChanged += new System.EventHandler(this.txt100_TextChanged);
+            // 
+            // txt50
+            // 
+            this.txt50.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt50.Location = new System.Drawing.Point(133, 147);
+            this.txt50.Name = "txt50";
+            this.txt50.Size = new System.Drawing.Size(140, 35);
+            this.txt50.TabIndex = 3;
+            this.txt50.TextChanged += new System.EventHandler(this.txt50_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "50";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt20
+            // 
+            this.txt20.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt20.Location = new System.Drawing.Point(133, 188);
+            this.txt20.Name = "txt20";
+            this.txt20.Size = new System.Drawing.Size(140, 35);
+            this.txt20.TabIndex = 5;
+            this.txt20.TextChanged += new System.EventHandler(this.txt20_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "20";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt10
+            // 
+            this.txt10.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt10.Location = new System.Drawing.Point(133, 229);
+            this.txt10.Name = "txt10";
+            this.txt10.Size = new System.Drawing.Size(140, 35);
+            this.txt10.TabIndex = 7;
+            this.txt10.TextChanged += new System.EventHandler(this.txt10_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(48, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "10";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl10
+            // 
+            this.lbl10.AutoSize = true;
+            this.lbl10.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl10.Location = new System.Drawing.Point(279, 235);
+            this.lbl10.Name = "lbl10";
+            this.lbl10.Size = new System.Drawing.Size(0, 29);
+            this.lbl10.TabIndex = 11;
+            this.lbl10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl20
+            // 
+            this.lbl20.AutoSize = true;
+            this.lbl20.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl20.Location = new System.Drawing.Point(279, 194);
+            this.lbl20.Name = "lbl20";
+            this.lbl20.Size = new System.Drawing.Size(0, 29);
+            this.lbl20.TabIndex = 10;
+            this.lbl20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl50
+            // 
+            this.lbl50.AutoSize = true;
+            this.lbl50.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl50.Location = new System.Drawing.Point(279, 153);
+            this.lbl50.Name = "lbl50";
+            this.lbl50.Size = new System.Drawing.Size(0, 29);
+            this.lbl50.TabIndex = 9;
+            this.lbl50.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl100
+            // 
+            this.lbl100.AutoSize = true;
+            this.lbl100.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl100.Location = new System.Drawing.Point(279, 112);
+            this.lbl100.Name = "lbl100";
+            this.lbl100.Size = new System.Drawing.Size(0, 29);
+            this.lbl100.TabIndex = 8;
+            this.lbl100.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(48, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 29);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Total";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txttotal
+            // 
+            this.txttotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttotal.Location = new System.Drawing.Point(133, 276);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.ReadOnly = true;
+            this.txttotal.Size = new System.Drawing.Size(140, 35);
+            this.txttotal.TabIndex = 13;
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(284, 321);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 49);
+            this.btnupdate.TabIndex = 14;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(122, 321);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 49);
+            this.btnclear.TabIndex = 15;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btninsert
+            // 
+            this.btninsert.Location = new System.Drawing.Point(203, 321);
+            this.btninsert.Name = "btninsert";
+            this.btninsert.Size = new System.Drawing.Size(75, 49);
+            this.btninsert.TabIndex = 16;
+            this.btninsert.Text = "Insert";
+            this.btninsert.UseVisualStyleBackColor = true;
+            this.btninsert.Click += new System.EventHandler(this.btninsert_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.Location = new System.Drawing.Point(436, 147);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.Size = new System.Drawing.Size(772, 255);
+            this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1220, 100);
+            this.panel1.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(124, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(401, 42);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Enter The Name Here";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CollectMoney.Properties.Resources.iconfinder_7_Picture_4906538;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblserach
+            // 
+            this.lblserach.AutoSize = true;
+            this.lblserach.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblserach.Location = new System.Drawing.Point(431, 109);
+            this.lblserach.Name = "lblserach";
+            this.lblserach.Size = new System.Drawing.Size(188, 29);
+            this.lblserach.TabIndex = 19;
+            this.lblserach.Text = "Search by Date :";
+            this.lblserach.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(625, 106);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(167, 35);
+            this.dateTimePicker1.TabIndex = 20;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1220, 422);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblserach);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btninsert);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbl10);
+            this.Controls.Add(this.lbl20);
+            this.Controls.Add(this.lbl50);
+            this.Controls.Add(this.lbl100);
+            this.Controls.Add(this.txt10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt20);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt50);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt100);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(1236, 461);
+            this.MinimumSize = new System.Drawing.Size(1236, 461);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt100;
+        private System.Windows.Forms.TextBox txt50;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt20;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt10;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl10;
+        private System.Windows.Forms.Label lbl20;
+        private System.Windows.Forms.Label lbl50;
+        private System.Windows.Forms.Label lbl100;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btninsert;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblserach;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ CollectMoney/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CollectMoney
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loadgrid();
+            Total();
+        }
+        int x100, x50, x20, x10, xtotal = 0;
+        int t100, t50, t20, t10;
+        string id ;
+        DateTime date =DateTime.Now;
+        DateTime time = DateTime.Now;
+        
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Collect;Integrated Security=True");
+
+        private void btninsert_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand insert = new SqlCommand("insert into collect (x100,x50,x20,x10,xtotal,cdate,ctime) values ('" + x100 + "','" + x50 + "','" + x20 + "','" + x10 + "','" + xtotal + "','" + date.ToString("dd/MM/yyyy") + "','" + time.ToString("hh:mm:ss") + "')", con);
+            insert.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Record has been successfully Inserted !!");
+            loadgrid();
+        }
+        void loadgrid()
+        {
+            con.Open();
+            SqlCommand select = new SqlCommand("select * from collect ", con);
+            SqlDataAdapter dataadpter = new SqlDataAdapter(select);
+            DataTable dt = new DataTable();
+            dataadpter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["id"].Visible = false;
+            con.Close();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //MessageBox.Show("hi");
+            if(e.RowIndex >= 0)
+            {
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+                txt100.Text = row.Cells["x100"].Value.ToString();
+                txt50.Text = row.Cells["x50"].Value.ToString();
+                txt20.Text = row.Cells["x20"].Value.ToString();
+                txt10.Text = row.Cells["x10"].Value.ToString();
+                id = row.Cells["id"].Value.ToString();
+
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand select = new SqlCommand("select * from collect where cdate = '" + dateTimePicker1.Value.ToString("dd/MM/yyyy") + "' ", con);
+            SqlDataAdapter dataadpter = new SqlDataAdapter(select);
+            DataTable dt = new DataTable();
+            dataadpter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["id"].Visible = false;
+            con.Close();
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            
+            SqlCommand update = new SqlCommand("update collect set x100='" + x100 + "',x50='" + x50 + "',x20='" + x20 + "',x10='" + x10 + "',xtotal='" + xtotal + "',cdate='" + date.ToString("dd/MM/yyyy") + "',ctime='" + time.ToString("hh:mm:ss") + "' where id='"+id+"'", con);
+            update.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Record has been successfully updated !!");
+            loadgrid();
+
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txt100.Text = ""; txt50.Text = ""; txt20.Text = ""; txt10.Text = "";
+            txttotal.Text = ""; lbl100.Text = ""; lbl50.Text = ""; lbl20.Text = "";
+            lbl10.Text = ""; 
+
+        }
+
+        void Total()
+        {
+            xtotal = t100 + t50 + t20 + t10;
+            txttotal.Text = xtotal.ToString();
+        }
+        private void txt10_TextChanged(object sender, EventArgs e)
+        {
+            try
+            { 
+            x10 = Convert.ToInt32(txt10.Text);
+            t10 = 10 * x10;
+            lbl10.Text = (t10).ToString();
+            Total();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void txt20_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                x20 = Convert.ToInt32(txt20.Text);
+                t20 = 20 * x20;
+                lbl20.Text = (t20).ToString();
+                Total();
+            
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void txt50_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                x50 = Convert.ToInt32(txt50.Text);
+                t50 = 50 * x50;
+                lbl50.Text = (t50).ToString();
+                Total();
+            
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void txt100_TextChanged(object sender, EventArgs e)
+        {
+            try {
+                x100 = Convert.ToInt32(txt100.Text);
+                t100 = 100 * x100;
+                lbl100.Text = (t100).ToString();
+                Total();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
